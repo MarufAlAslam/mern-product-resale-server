a0ba12351efd35f259cb45994a20456f5a7fad75
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-11-24 12:56:44 +0600

    categories API created

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
+
+require('dotenv').config()
+
+
 const app = express()
 const port = process.env.port || 5000
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pjynjtn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// console.log(uri)
+
+async function run() {
+    try {
+        // await client.connect();
+        const categoryCollection = client.db("resellStore").collection("categories");
+
+
+        // get all categories from category collection
+        app.get('/categories', async (req, res) => {
+            const cursor = categoryCollection.find({});
+            const categories = await cursor.toArray();
+            // console.log(categories)
+            res.send(categories)
+        })
+
+
+
+    } finally {
+    }
+}
+
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
     res.send('e-trade server running')
